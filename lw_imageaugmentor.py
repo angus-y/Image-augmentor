2679d637134da21ed6b5e22f11915c1ac9979af2
liw3n <76936050+liw3n@users.noreply.github.com>
2021-01-11 10:27:31 +0800

    Update lw_imageaugmentor.py
    accept both lowercase and uppercase

--- lw_imageaugmentor.py
+++ lw_imageaugmentor.py
@@ -11,10 +11,10 @@ def random_rotate(image, angle=None):
     ranrotate_image.show()
 
 def flip(image, direction):
-    if direction == 'Left Right':
+    if direction.upper() == 'LEFT RIGHT':
         flip_lr = image.transpose(Image.FLIP_LEFT_RIGHT)
         flip_lr.show()
-    if direction == 'Top Bottom':
+    if direction.upper() == 'TOP BOTTOM':
         flip_tb = image.transpose(Image.FLIP_TOP_BOTTOM)
         flip_tb.show()
 
@@ -22,10 +22,10 @@ def random_flip(image, direction=None):
     direction_types = ['Left Right', 'Top Bottom']
     random_number = random.randint(1, len(direction_types)) - 1
     direction = direction_types[random_number]
-    if direction == 'Left Right':
+    if direction.upper == 'LEFT RIGHT':
         flip_lr = image.transpose(Image.FLIP_LEFT_RIGHT)
         flip_lr.show()
-    if direction == 'Top Bottom':
+    if direction.upper() == 'TOP BOTTOM':
         flip_tb = image.transpose(Image.FLIP_TOP_BOTTOM)
         flip_tb.show()
 
@@ -33,29 +33,44 @@ def crop(image, box):
     cropped_image = image.crop(box)
     cropped_image.show()
 
-def blur(image, type):
-    if type == 'Blur':
+def blur(image, type, radius):
+    if type.upper() == 'BLUR':
+        blurred_image = image.filter(ImageFilter.BLUR())
+        blurred_image.show()
+    if type.upper == 'GAUSSIAN BLUR':
+        blurred_image = image.filter(ImageFilter.GaussianBlur(radius))
+        blurred_image.show()
+    if type.upper() == 'BOX BLUR':
+        blurred_image = image.filter(ImageFilter.BoxBlur(radius))
+        blurred_image.show()
+
+def random_blur(image, max_radius, type = None):
+    type = ['Blur', 'Gaussian Blur', 'Box Blur']
+    type_number = random.randint(1, len(type)) - 1
+    type = type[type_number]
+    radius = random.randint(1, max_radius)
+
+    if type.upper() == 'BLUR':
         blurred_image = image.filter(ImageFilter.BLUR)
         blurred_image.show()
-    if type == 'Gaussian Blur':
-        blurred_image = image.filter(ImageFilter.GaussianBlur())
+    if type.upper() == 'GAUSSIAN BLUR':
+        blurred_image = image.filter(ImageFilter.GaussianBlur(radius))
         blurred_image.show()
-    if type == 'Box Blur':
-        blurred_image = image.filter(ImageFilter.BoxBlur())
+    if type.upper() == 'BOX BLUR':
+        blurred_image = image.filter(ImageFilter.BoxBlur(radius))
         blurred_image.show()
 
 def enhance(image, type, amount):
-
-    if type == 'Brightness':
+    if type.upper() == 'BRIGHTNESS':
         enhanced_img = ImageEnhance.Brightness(image)
         enhanced_img.enhance(amount).show()
-    if type == 'Colour':
+    if type.upper() == 'COLOR':
         enhanced_img = ImageEnhance.Color(image)
         enhanced_img.enhance(amount).show()
-    if type == 'Contrast':
+    if type.upper() == 'CONTRAST':
         enhanced_img = ImageEnhance.Contrast(image)
         enhanced_img.enhance(amount).show()
-    if type == 'Sharpness':
+    if type.upper() == 'SHARPNESS':
         enhanced_img = ImageEnhance.Sharpness(image)
         enhanced_img.enhance(amount).show()
 
@@ -65,16 +80,16 @@ def random_enhance(image, max_amount, type=None):
     type_number = random.randint(1, len(type)) - 1
     type = type[type_number]
 
-    if type == 'Brightness':
+    if type.upper() == 'BRIGHTNESS':
         enhanced_img = ImageEnhance.Brightness(image)
         enhanced_img.enhance(amount).show()
-    if type == 'Colour':
+    if type.upper() == 'COLOR':
         enhanced_img = ImageEnhance.Color(image)
         enhanced_img.enhance(amount).show()
-    if type == 'Contrast':
+    if type.upper() == 'CONTRAST':
         enhanced_img = ImageEnhance.Contrast(image)
         enhanced_img.enhance(amount).show()
-    if type == 'Sharpness':
+    if type.upper() == 'SHARPNESS':
         enhanced_img = ImageEnhance.Sharpness(image)
         enhanced_img.enhance(amount).show()
 
@@ -84,20 +99,19 @@ def colourbands_inversion(image):
     image.show()
 
 def mask(image, mask_shape, xy):
-    if mask_shape == 'Rectangle':
+    if mask_shape.upper() == 'RECTANGLE':
         mask = Image.new("L", image.size, 0)
         draw = ImageDraw.Draw(mask)
         draw.rectangle(xy, 255)
         masked_image = Image.composite(image, mask, mask)
         masked_image.show()
     
-    if mask_shape == 'Ellipse':
+    if mask_shape.upper() == 'ELLIPSE':
         mask = Image.new("L", image.size, 0)
         draw = ImageDraw.Draw(mask)
         draw.ellipse(xy, 255)
         masked_image = Image.composite(image, mask, mask)
         masked_image.show()
-
 
 
 
